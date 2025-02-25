b74132b6f2302b47d1e6ef4c048e235c783152f1
Mahmudul Alam <mahmudula2000@gmail.com>
2025-02-25 16:59:00 +0600

    added login with facebook

--- prisma/auth.ts
+++ prisma/auth.ts
@@ -2,8 +2,9 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/prisma/client";
 import Google from "@auth/core/providers/google";
+import Facebook from "@auth/core/providers/facebook";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  providers: [Google, Facebook],
 });

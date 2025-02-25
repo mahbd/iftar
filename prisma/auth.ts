e8075dc6ee51a566a77ae7713a4c727ebff08712
Mahmudul Alam <mahmudula2000@gmail.com>
2025-02-25 17:19:12 +0600

    removed login with facebook

--- prisma/auth.ts
+++ prisma/auth.ts
@@ -2,9 +2,8 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/prisma/client";
 import Google from "@auth/core/providers/google";
-import Facebook from "@auth/core/providers/facebook";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google, Facebook],
+  providers: [Google],
 });
